6dedc7fe6d0c1b8fc6eaa46c3eef411a0932fc59
carmenfu006 <guess_006@hotmail.com>
2021-06-23 11:04:56 +0800

    Setup user devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  # This devise helper method applies to after sign up and login
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
 end
