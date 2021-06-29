8d71eb94023b9cc4cb62754b15a348b980b0921d
carmenfu006 <guess_006@hotmail.com>
2021-06-29 11:54:29 +0800

    Add stimulus js example

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,34 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
+
+  static targets = [ "name", "output" ]
+
   connect() {
-    this.element.textContent = "Hello World!"
+    // this.element.textContent = "Hello World!"
+    // console.log(this.element)
+    // console.log(this.world)
+    // console.log(this.outputTargets)
+    // this.outputTargets.forEach((elem) => {
+    //   console.log(elem.textContent)
+    // })
+    // this.outputTarget.textContent = 'Welcome to Rambutan Code Academy!'
+  }
+
+  greet(event) {
+    this.outputTarget.textContent =
+      `Hello, ${this.nameTarget.value}!`
+
+    // this.outputTargets.forEach((elem) => {
+    //   elem.textContent =
+    //   `Hello, ${this.nameTarget.value}!`
+    // })
+
+    this.data.set('world', 'haha')
+    console.log(event.currentTarget.dataset.color)
+  }
+
+  get world() {
+    return this.data.get('world')
   }
 }
