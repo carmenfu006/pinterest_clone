926ccb96257d14f6601da73510e076088ba76c90
carmenfu006 <guess_006@hotmail.com>
2021-07-02 10:41:01 +0800

    Change the way to add comment to pin

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -8,16 +8,22 @@ class Comment < ApplicationRecord
 
   validates :body, presence: true
 
-  after_create_commit do
-    broadcast_append_later_to [pin, :comments], target: "#{dom_id(pin)}_comments", locals: { user: self.user }
-  end
+  # after_create_commit do
+  #   broadcast_append_later_to [pin, :comments], target: "#{dom_id(pin)}_comments", locals: { user: self.user }
+  # end
 
-  after_destroy_commit do
-    broadcast_remove_to [pin, :comments]
-  end
+  # after_destroy_commit do
+  #   broadcast_remove_to [pin, :comments]
+  # end
 
   def liked(user)
     like = likes.find_by_user_id(user.id)
     return like if like.present?
   end
+
+  after_create_commit do
+     broadcast_append_to pin, locals: { user: self.user, pin: pin }
+  end
+
+  after_destroy_commit { broadcast_remove_to pin }
 end
