38e3681bebcc89d3a785d13c4eaedfcc6590a2f2
carmenfu006 <guess_006@hotmail.com>
2021-07-13 13:58:01 +0800

    Add flash message to comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -54,7 +54,8 @@ class CommentsController < ApplicationController
     @comment.destroy
     respond_to do |format|
       # format.turbo_stream { }
-      format.turbo_stream { render turbo_stream: turbo_stream.remove(@comment) }
+      @flash = turbo_flash(notice: 'Comment was successfully destroyed')
+      format.turbo_stream { render turbo_stream: @flash + turbo_stream.remove(@comment) }
       format.html { redirect_to pin_path(@pin) }
     end
   end
