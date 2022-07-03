5bb5f80c4f75a657928e2a314df40c46165deb31
carmenfu006 <guess_006@hotmail.com>
2022-07-03 17:06:14 +0800

    Update on 03JUL2022

--- app/models/board.rb
+++ app/models/board.rb
@@ -8,4 +8,10 @@ class Board < ApplicationRecord
   after_create_commit { broadcast_append_to 'boards', target: "user_#{self.user.id}_boards" }
   after_update_commit { broadcast_replace_to 'boards' }
   # after_destroy_commit { broadcast_remove_to 'boards' }
+
+
+  # <%= turbo_stream_from 'boards' %>
+  # after_create_commit { broadcast_append_to 'boards', target: "board_list" }
+  # after_update_commit { broadcast_append_to 'boards', target: "board_list" }
+  # after_destroy_commit { broadcast_remove_to 'boards'}
 end
